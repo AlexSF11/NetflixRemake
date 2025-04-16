a38ae7665663e999e133147c38547aa925c1dce9
AlexSF11 <alexfreire836@gmail.com>
2025-04-16 10:58:27 -0300

    Parsers: Convertendo Estruturas de Dados

--- app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/util/CategoryTask.kt
@@ -1,12 +1,16 @@
 package co.tiagoaguiar.netflixremake.util
 
 import android.util.Log
+import co.tiagoaguiar.netflixremake.model.Category
+import co.tiagoaguiar.netflixremake.model.Movie
+import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import java.nio.Buffer
 import java.util.concurrent.Executors
 
 class CategoryTask {
@@ -18,11 +22,12 @@ class CategoryTask {
         val executor = Executors.newSingleThreadExecutor()
 
         executor.execute{
+            var urlConnection: HttpURLConnection? = null
+            var stream: InputStream? = null
             try {
                 // Nesse momento, estamos utilizando a NOVA-thread (Processo paralelo) [2]
                 val requestURL = URL(url) // Abrir uma URL
-                val urlConnection =
-                    requestURL.openConnection() as HttpURLConnection // Abrir uma conexão
+                urlConnection = requestURL.openConnection() as HttpURLConnection // Abrir uma conexão
                 urlConnection.readTimeout = 2000 // Tempo de leitura (2s)
                 urlConnection.connectTimeout = 2000 // Tempo de conexão (2s)
 
@@ -31,34 +36,47 @@ class CategoryTask {
                     throw  IOException("Erro na comunicação com o servidor!")
                 }
 
-                val stream = urlConnection.inputStream // Sequencia de bytes
+                stream = urlConnection.inputStream // Sequencia de bytes
 
                 // Forma 1: simples e rápida
-                //val jsonAsString = strem.bufferedReader().use { it.readText() } // Bytes -> Strings
+                val jsonAsString = stream.bufferedReader().use { it.readText() } // Bytes -> Strings
 
-                // Forma 2
-                val buffer = BufferedInputStream(stream)
-                val jsonAsString = toString(buffer)
-
-                Log.i("Teste", jsonAsString)
+                val categories = toCategories(jsonAsString)
+                Log.i("Teste", categories.toString())
 
             } catch (e: IOException) {
                 Log.e("Teste", e.message ?: "Erro desconhecido", e)
+            } finally {
+                urlConnection?.disconnect()
+                stream?.close()
             }
         }
     }
 
-    private fun toString(stream: InputStream) : String {
-        val bytes = ByteArray(1024)
-        val baos = ByteArrayOutputStream()
-        var read: Int
-        while(true) {
-            read = stream.read(bytes)
-            if (read <= 0) {
-                break
+    private fun toCategories(jsonAsString: String) : List<Category> {
+        val categories = mutableListOf<Category>()
+
+        val jsonRoot = JSONObject(jsonAsString)
+        val jsonCategories = jsonRoot.getJSONArray("category")
+        for (i in 0 until jsonCategories.length()) {
+            val jsonCategory = jsonCategories.getJSONObject(i)
+
+            val title = jsonCategory.getString("title")
+            val jsonMovies = jsonCategory.getJSONArray("movie")
+
+            val movies = mutableListOf<Movie>()
+            for (j in 0 until jsonMovies.length()) {
+                val jsonMovie = jsonMovies.getJSONObject(j)
+                val id = jsonMovie.getInt("id")
+                val coverUrl = jsonMovie.getString("cover_url")
+
+                movies.add(Movie(id, coverUrl))
             }
-            baos.write(bytes, 0, read)
+
+            categories.add(Category(title, movies))
         }
-        return String(baos.toByteArray())
+
+        return categories
     }
+
 }
